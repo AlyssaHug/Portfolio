5e5461bd4119581e70970775b9039b6c4557a2db
AlyssaHug <AlyssaHug@shaw.ca>
2026-01-09 21:38:44 -0800

    initial file push

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className='header'>
+                <h1>Hi! I'm Alyssa.</h1>
+            </div>
+            <div className='content'>
+                <p>
+                    My site is currently under construction. Working hard to
+                    have it updated soon!
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default App;
