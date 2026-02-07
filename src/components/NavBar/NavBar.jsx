58181e679b050dde0f12e9c3eba81e67b5691eed
AlyssaHug <AlyssaHug@shaw.ca>
2026-02-06 20:56:01 -0800

    adjust navbar styles + add dropdown for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,118 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import Portfolio from "../../pages/Portfolio/Portfolio";
 import "./NavBar.css";
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
+    const closeMenu = () => setIsOpen(false);
+
     return (
-        <div className='nav-bar'>
-            <NavLink
-                to='/'
-                className='link-img'>
-                <img
-                    src='/LogoBlue.svg'
-                    alt='Logo - Return to Home'
-                    className='nav-logo'
-                />
-            </NavLink>
-            <div className='nav-items'>
+        <div className='nav-container'>
+            <div className='nav-bar'>
+                {/* Left - Logo + Name */}
+
                 <NavLink
-                    to='/Portfolio'
-                    className={({ isActive }) =>
-                        isActive ? "nav-link active" : "nav-link"
-                    }>
-                    Portfolio
-                </NavLink>
-                <NavLink
-                    to='/About'
-                    className={({ isActive }) =>
-                        isActive ? "nav-link active" : "nav-link"
-                    }>
-                    About
+                    to='/'
+                    className='link-img'
+                    onClick={closeMenu}>
+                    <img
+                        src='/LogoBlue.svg'
+                        alt='Logo - Return to Home'
+                        className='nav-logo'
+                    />
+                    <p className='nav-name'>lyssa Huggins</p>
                 </NavLink>
 
-                <a
-                    href='mailto:alyssathuggins@gmail.com'
-                    className='nav-link'>
-                    Get in touch!
-                </a>
+                {/* Desktop Nav Links - hidden on mobile */}
+                <div className='nav-items desktop-nav'>
+                    <NavLink
+                        to='/Portfolio'
+                        className={({ isActive }) =>
+                            isActive ? "nav-link active" : "nav-link"
+                        }
+                        onClick={closeMenu}>
+                        Portfolio
+                    </NavLink>
+                    <NavLink
+                        to='/About'
+                        className={({ isActive }) =>
+                            isActive ? "nav-link active" : "nav-link"
+                        }
+                        onClick={closeMenu}>
+                        About
+                    </NavLink>
+                    <NavLink
+                        to='/Gallery'
+                        className={({ isActive }) =>
+                            isActive ? "nav-link active" : "nav-link"
+                        }
+                        onClick={closeMenu}>
+                        Gallery
+                    </NavLink>
+                </div>
+
+                {/* Desktop Contact Button - hidden on mobile */}
+                <div className='connect-btn desktop-nav'>
+                    <a
+                        href='mailto:alyssathuggins@gmail.com'
+                        className='nav-item'
+                        onClick={closeMenu}>
+                        Get in touch!
+                    </a>
+                </div>
+
+                {/* Hamburger Button - visible only on mobile */}
+                <button
+                    className='hamburger'
+                    onClick={toggleMenu}
+                    aria-label='Toggle menu'
+                    aria-expanded={isOpen}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                {/* Mobile Menu Dropdown */}
+                <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
+                    <NavLink
+                        to='/Portfolio'
+                        className={({ isActive }) =>
+                            isActive
+                                ? "nav-link active mobile-link"
+                                : "nav-link mobile-link"
+                        }
+                        onClick={closeMenu}>
+                        Portfolio
+                    </NavLink>
+                    <NavLink
+                        to='/About'
+                        className={({ isActive }) =>
+                            isActive
+                                ? "nav-link active mobile-link"
+                                : "nav-link mobile-link"
+                        }
+                        onClick={closeMenu}>
+                        About
+                    </NavLink>
+                    <NavLink
+                        to='/Gallery'
+                        className={({ isActive }) =>
+                            isActive
+                                ? "nav-link active mobile-link"
+                                : "nav-link mobile-link"
+                        }
+                        onClick={closeMenu}>
+                        Gallery
+                    </NavLink>
+
+                    <a
+                        href='mailto:alyssathuggins@gmail.com'
+                        className='nav-item mobile-link'
+                        onClick={closeMenu}>
+                        Get in touch!
+                    </a>
+                </div>
             </div>
         </div>
     );
